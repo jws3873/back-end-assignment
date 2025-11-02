6bdfdb7c745e8b4053d4450220e27f63d08dc9f6
jungwonseok <dnjsehfdld@naver.com>
2025-11-02 13:32:40 +0900

    [feat] 상품 API 및 테스트(단위, 통합) 코드 구현

---
+++ src/main/java/com/example/shoppingmall/product/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.example.shoppingmall.product.repository;
+
+import com.example.shoppingmall.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom { }
